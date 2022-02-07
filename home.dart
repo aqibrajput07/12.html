cbda530c393cdc87e65feabf85fc98ebb9681870
aqibrajput07 <98934039+aqibrajput07@users.noreply.github.com>
2022-02-07 09:46:59 +0500

    add

---
+++ home.dart
@@ -0,0 +1,65 @@
+void main () {
+ 
+ // print()=>predefined  Function =>functionname(sssss)
+  // Comment=> (ctrl+/)
+  // Function Type: Predefine Function(build in),UserDefine Function
+  // \n=>new line(escape Character)
+  // \t=>tab(5 spaces)
+//  print("Hello ");
+//  print('World');
+//  print("Hello\nWorld\nclass 2\nfriday ");
+//  print("Hello");
+//  print("World");
+//  print("Class 2");
+//  print("friday");
+// print("Class 2                         Friday");
+// print("class 3 \tClass 4");
+// print("Asad");
+// print("Khan");
+// print("Data");
+// print("Asad");
+// print("Khan");
+// print("Asad");
+// print("Khan");
+// print("Asad");
+// print("Khan");
+
+// varaible=> in memory location store value
+// how to make Variable
+// Rule For MAKE vRAAIBLE
+// NOT START WITH NUMBER
+// NOT START WITH SPECIAL CHARCTER BUT EXCEPT $
+// NOT SPACE BETWEEN THEM VN
+//NUMBER 
+// Datatype=>which type of data store in this varaible
+// var d_ear=123;
+// Declare,inlinlization
+// int a; //Declare Variale
+// int b=12; //Inlinlization
+// Syntax
+// Datatype vraibale;
+// 1)Number =>int,double,num
+// int a1=12; //int not accept point value
+// double a2=14265362535.4554;
+// num a3 = 1253656253652.726472;
+// print(a1);
+// print(a2);
+// print(a3);
+
+// 2)String=>collection of words/letter
+// String b1='Asad sbdhgs shgdhgsfd gsfdgfsgd sgdfgsfdg sgdgs14324132 tert tqrtwr52.2626732';
+// String b3='Omer';
+// print(b1);
+// print(b3);
+
+// 3)boolean =>true,false
+bool a=false;
+print(a);
+
+
+
+
+
+
+
+}
